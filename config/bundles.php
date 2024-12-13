a00482f5ab88893d7125f88df84bb776ac29b1d5
Soner Sayakci <s.sayakci@gmail.com>
2024-12-13 14:42:59 +0100

    initial commit

---
+++ config/bundles.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Shopware\Core\Profiling\Profiling::class => ['all' => true],
+    Shopware\Core\Framework\Framework::class => ['all' => true],
+    Shopware\Core\System\System::class => ['all' => true],
+    Shopware\Core\Content\Content::class => ['all' => true],
+    Shopware\Core\Checkout\Checkout::class => ['all' => true],
+    Shopware\Core\Maintenance\Maintenance::class => ['all' => true],
+    Shopware\Administration\Administration::class => ['all' => true],
+    Shopware\Storefront\Storefront::class => ['all' => true],
+    Shopware\Core\Service\Service::class => ['all' => true],
+];
